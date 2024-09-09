a36d7c43974c9a1e0d10bdd4f5b22718991a3417
stoffe-fe23 <kristoffer.bengtsson@gritacademy.se>
2024-09-09 12:47:28 +0200

    Produktsökning stomme

--- js/productsearch.js
+++ js/productsearch.js
@@ -15,18 +15,21 @@ if (searchForm) {
             resultsBox.innerHTML = "";
             if (searchResult.length) {
                 for (const product of searchResult) {
+                    // Create a new product card for this product
                     const card = template.content.firstElementChild.cloneNode(true);
                     const name = card.querySelector(".search-product-name");
                     const desc = card.querySelector(".search-product-desc");
                     const link = card.querySelector(".search-product-link");
                     const image = card.querySelector(".search-product-image");
 
+                    // Fill in product data on the card elements.
                     name.innerText = product.name;
                     desc.innerText = product.description;
                     card.setAttribute("data-productid", product.productid);
                     link.href = `productinfo.html?product=${product.productid}`;
                     image.src = product.image[0];
 
+                    // Add product card to search results. 
                     resultsBox.appendChild(card);
                 }
             }
