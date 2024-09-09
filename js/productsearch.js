3c60dae9e11c3d473889f3b0fe33a9b387bbc386
stoffe-fe23 <kristoffer.bengtsson@gritacademy.se>
2024-09-09 13:43:21 +0200

    Produktsökning stomme

--- js/productsearch.js
+++ js/productsearch.js
@@ -9,22 +9,12 @@ if (searchForm) {
         const searchFor = formData.get("search-text");
         if (searchFor && searchFor.length) {
             const resultsBox = document.querySelector("#product-search-result");
-            const template = document.querySelector("#product-search-result-template");
             const searchResult = await productdata.searchProducts(searchFor);
 
             resultsBox.innerHTML = "";
             if (searchResult.length) {
                 for (const product of searchResult) {
-                    // Create a new product card for this product
-                    const card = template.content.firstElementChild.cloneNode(true);
-                    card.querySelector(".search-product-name").innerText = product.name;;
-                    card.querySelector(".search-product-desc").innerText = product.description;
-                    card.querySelector(".search-product-link").href = `productinfo.html?product=${product.productid}`;
-                    card.querySelector(".search-product-image").src = product.image[0];
-                    card.setAttribute("data-productid", product.productid);
-
-                    // Add product card to search results. 
-                    resultsBox.appendChild(card);
+                    resultsBox.appendChild(createProductCard(product));
                 }
             }
             else {
@@ -34,4 +24,15 @@ if (searchForm) {
         }
 
     });
+}
+
+function createProductCard(product) {
+    const template = document.querySelector("#product-search-result-template");
+    const card = template.content.firstElementChild.cloneNode(true);
+    card.querySelector(".search-product-name").innerText = product.name;
+    card.querySelector(".search-product-desc").innerText = product.description;
+    card.querySelector(".search-product-link").href = `productinfo.html?product=${product.productid}`;
+    card.querySelector(".search-product-image").src = product.image[0];
+    card.setAttribute("data-productid", product.productid);
+    return card;
 }
